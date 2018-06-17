71e761802f1c8e807e3a8f46f98c77d11200c6f7
Daniel Starzmann <daniel@starze.de>
2018-06-17 20:57:40 +0200

    added support for arm32v7, optional update of ipv4/ipv6 and auto-reneval every 5 min.

--- README.md
+++ README.md
@@ -3,8 +3,31 @@
 ## description
 Dynv6-client is a client for [dynv6](https://dynv6.com/)
 
+Supports x86 and arm based devices (i.e. raspberry pi).
 
-## Usage
+## start x86 container
 ```bash
-$ docker run --network=host --rm ticosax/dynv6-client:latest --token=<your-token> --interface=eth0 example.dynv6.net
+$ docker run --network=host --rm starze/dynv6-client:latest --token=<your-token> --interface=eth0 [--no-ipv4] [--no-ipv6] example.dynv6.net
 ```
+
+## start arm container
+```bash
+$ docker run --network=host --rm starze/arm32v7-dynv6-client:latest --token=<your-token> --interface=eth0 [--no-ipv4] [--no-ipv6] example.dynv6.net
+```
+
+## build x86 container
+```bash
+$ docker build -t dynv6-client .
+```
+
+## build arm container
+```bash
+$ docker build -t arm32v7-dynv6-client -f Dockerfile.arm32v7 .
+```
+
+# Dockerfile and Code
+https://github.com/starze/dynv6-client
+
+
+
+Special thanks to https://github.com/ticosax/dynv6-client for making this possible!
--- client.py
+++ client.py
@@ -4,22 +4,31 @@ import certifi  # noqa
 import click
 import netifaces
 import requests
-
+import time
 
 @click.command()
 @click.argument('hostname')
 @click.option('--token', prompt=True, hide_input=True)
 @click.option('--interface', default='eth0')
-def main(hostname, token, interface):
-    addresses = netifaces.ifaddresses(interface)
-    ipv6 = addresses[netifaces.AF_INET6][0]['addr']
-    ipv4 = requests.get('https://api.ipify.org?format=json').json()['ip']
-    parameters = {'hostname': hostname, 'token': token, 'ipv6': ipv6,
-                  'ipv4': ipv4}
-    response = requests.get('https://dynv6.com/api/update?{}'.format(
-        urllib.parse.urlencode(parameters)))
-    assert response.status_code == 200, response.content
+@click.option('--ipv4/--no-ipv4', default=True)
+@click.option('--ipv6/--no-ipv6', default=True)
+def main(hostname, token, interface, ipv4, ipv6):
+    while 1:
+        addresses = netifaces.ifaddresses(interface)
+        parameters = {'hostname': hostname, 'token': token }
 
+        if ipv4:
+            ipv4addr = requests.get('https://api.ipify.org?format=json').json()['ip']
+            parameters['ipv4'] = ipv4addr
+
+        if ipv6:
+            ipv6addr = addresses[netifaces.AF_INET6][0]['addr']
+            parameters['ipv6'] = ipv6addr
+
+        response = requests.get('https://dynv6.com/api/update?{}'.format(
+            urllib.parse.urlencode(parameters)))
+        assert response.status_code == 200, response.content
+        time.sleep(5 * 60)
 
 if __name__ == '__main__':
     main()
